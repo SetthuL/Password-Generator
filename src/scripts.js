65c59e196c1329e89f21df98ae4312945ff2fa9a
Lange Siphosethu <128488997+SetthuL@users.noreply.github.com>
2025-03-04 15:34:05 +0200

    Updated js

--- src/scripts.js
+++ src/scripts.js
@@ -1,8 +1,8 @@
 function generatePassword() {
 
     let length = document.getElementById("length").value;
-    let includeNumbers = document.getElementById("includeNumbers").Checked;
-    let includeSymbols = document.getElementById("includeSymbols").Checked;
+    let includeNumbers = document.getElementById("includeNumbers").checked;
+    let includeSymbols = document.getElementById("includeSymbols").checked;
 
     let chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
     if (includeNumbers) chars += "0123456789";
@@ -16,3 +16,47 @@ function generatePassword() {
     updateStrength(password);
 }
 
+function copy_password() {
+    let password_field = document.getElementById("password");
+    password_field.select();
+    alert("Password copied!");
+}
+
+function updateStrength(password) {
+    let strength_bar = document.getElementById("strength_bar");
+    let strength_text = document.getElementById("strength_text");
+
+    let strength = 0;
+    if (password.length >= 8) strength++;
+    if (password.length >= 12) strength++;
+    if (/[0-9]/.test(password)) strength++;
+    if (/[!@#$%^&><?*;:+-]/.test(password)) strength++;
+
+    if (strength === 0)
+    {
+        strength_bar.style.width = "0%";
+        strength_bar.style.background = "gray";
+        strength_text.textContent = "Strength: Too Weak";
+    }
+
+    else if (strength === 1)
+    {
+        strength_bar.style.width = "33%";
+        strength_bar.style.background = "red";
+        strength_text.textContent = "Strength: Weak";
+    }
+
+    else if (strength === 2)
+    {
+        strength_bar.style.width = "66%";
+        strength_bar.style.background = "orange";
+        strength_text.textContent = "Strength: Medium";
+    }
+
+    else
+    {
+        strength_bar.style.width = "100%";
+        strength_bar.style.background = "green";
+        strength_text.textContent = "Strength: Strong";
+    }
+}
